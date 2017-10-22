f83342c5000e53c5785475b469e42a467fbb5086
Darnell W <darnellwillis96@gmail.com>
2017-10-22 00:08:57 -0700

    server

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
+var path = require("path");
 var app = express();
+var PORT = process.env.PORT || 8080; 
 
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+console.log("Yes");;
+app.listen(PORT, function() {
+    console.log("Please go to PORT: " + PORT);
+  });
 
