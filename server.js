6f7e9532aa0f95bea48f46489c102f04e0898262
Darnell W <darnellwillis96@gmail.com>
2017-10-22 19:49:54 -0700

    route

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ var PORT = process.env.PORT || 8080;
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.get('/', function(req, res) {
+  res.json("HELLO");
+});
+
 console.log("Yes");;
 app.listen(PORT, function() {
     console.log("Please go to PORT: " + PORT);
