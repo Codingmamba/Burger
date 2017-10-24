fb2395853e612de1801a1348ea9e27a9ff0cc026
Darnell W <darnellwillis96@gmail.com>
2017-10-24 00:50:34 -0700

    handlebars

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,7 +1,11 @@
 var express = require('express');
 let burg = require('../models/burger.js');
 var router = express.Router();
+var path = require('path');
 
+router.get("/", function(req, res) {
+    res.json(path.join(__dirname, "../views/index.handlebars"))
+}); 
 
 router.get('/burgers', function(req, res) {
     res.json("Testing");
--- server.js
+++ server.js
@@ -4,16 +4,12 @@ var methodOverride = require('method-override');
 var bodyParser = require('body-parser');
 var path = require("path");
 var app = express();
-var PORT = process.env.PORT || 8080; 
+var PORT = process.env.PORT || 3030; 
 
 app.use(methodOverride('_method'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-app.get('/', function(req, res) {
-  res.json("HELLO");
-});
 
 let routes = require('./controllers/burgers_controllers.js');
 app.use('/', routes);
@@ -22,5 +18,3 @@ console.log("Yes");;
 app.listen(PORT, function() {
     console.log("Please go to PORT: " + PORT);
   });
-
-app.listen(PORT);
