f210c0941c0548d1a0d45739a5364941a5b0b852
Darnell W <darnellwillis96@gmail.com>
2017-10-24 23:22:22 -0700

    connecting routes

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,14 +1,26 @@
 var express = require('express');
 let burg = require('../models/burger.js');
+var connection = require('../config/connection.js');
 var router = express.Router();
-var path = require('path');
+var path = require('path'); 
 
-router.get("/", function(req, res) {
-    res.json(path.join(__dirname, "../views/index.handlebars"))
-}); 
+router.get('/', function(req, res) {
+    connection.query("SELECT * FROM burgers;", function(err, data) {
+        if (err) {
+          return res.status(500).end();
+        }
+        //res.json(data);
+        res.render("index", { burgers: data });
+      });
+    });
 
-router.get('/burgers', function(req, res) {
-    res.json("Testing");
-});
+render.post("/", function(req, res) {
+    res.send('You sent, ' + req.body.task);
+    connection.query("INSERT INTO burgers (task) VALUES (?)", [req.body.task], function(err, result) {
+        if (err) throw err;
+    
+        res.redirect("/");
+      });
+    });
 
 module.exports = router;
